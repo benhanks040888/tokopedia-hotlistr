ce274f9c1df3e543de4b6ec14079de1844211d8e
Hendra Susanto <benhanks040888@gmail.com>
2018-04-23 19:25:27 +0700

    update css

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 const Card = ({item}) =>
-  <div className="max-w-md rounded overflow-hidden shadow-lg">
-    <img className="w-full"
-      src={item.img}
-      alt={item.title}
-    />
-    <div className="px-6 py-4">
-      <h2 className="font-bold text-lg mb-2">{item.title}</h2>
-      <p className="text-grey-darker text-base">
+  <a href={item.url} className="card" target="_blank">
+    <div className="card-image">
+      <img className="fit"
+        src={item.img}
+        alt={item.title}
+      />
+    </div>
+    <div className="card-body px-6 py-4">
+      <h2 className="card-title font-bold text-lg mb-2">{item.title}</h2>
+      <p className="card-subtitle text-grey-darker text-base">
         Starts from {item.price_start_from}
       </p>
     </div>
-  </div>
+  </a>
 
 export default Card;
